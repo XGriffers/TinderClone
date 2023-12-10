b30403972134997c7f5d13dd210aeb10382a47a6
Xgriffers <stevengriffith6@student.purdueglobal.edu>
2023-12-10 13:22:51 -0500

    frontend created with react and added navbar

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+
+import MainPage from "./Components/MainPage/MainPage";
+import Navigation from "./Components/Navigation/Navigation";
+
+
+function App() {
+  return(
+    <>
+    <Navigation />
+
+    <MainPage />
+    </>
+    );
+}
+
+export default App;
---
+++ frontend/src/Components/MainPage/MainPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import mainPage from '../../assets/mainPage.png'
+import './MainPage.css'
+
+const MainPage = () => {
+  return (
+    <img src={mainPage} alt="" className='mainImg'/>
+  )
+}
+
+export default MainPage;
